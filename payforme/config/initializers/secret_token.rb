5df07d6d3539f50d23b2b9c2386985157df7e00d
Mathias Fiedler <mathias@musterdenker.de>
2013-04-23 14:29:42 +0200

    init commit, empty rails app

---
+++ payforme/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Payforme::Application.config.secret_token = '8baa59108e1ea43bb2599649814ff0a7d3bca6c4ceb1ddff2918c19fefa1b41fffc975629aed80cc55d0dea27a013afd96b1d53a842c8dfd1cde41752da59751'
