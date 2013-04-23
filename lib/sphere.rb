cba23122a28b716e5d0e040235697fd82bab25b1
Hajo Eichler <hajo.eichler@commercetools.de>
2013-04-23 17:15:40 +0200

    Establish first contact to SPHERE.IO.

--- lib/sphere.rb
+++ lib/sphere.rb
@@ -1,3 +1,26 @@
+require 'excon'
+require 'json'
+
 module Sphere
-  
+
+  def self.login(client_id, client_secret, project_key)
+    encoded = Base64.urlsafe_encode64 "#{client_id}:#{client_secret}"
+    headers = { 'Authorization' => "Basic #{encoded}", 'Content-Type' => 'application/x-www-form-urlencoded' }
+    body = "grant_type=client_credentials&scope=manage_project:#{project_key}"
+    res = Excon.post 'https://auth.sphere.io/oauth/token', :headers => headers, :body => body
+    raise "Problems on getting access token from auth.sphere.io: #{res.body}" unless res.status == 200
+    JSON.parse(res.body)['access_token']
+  end
+
+  def self.get_order(access_token, project_key, order_id)
+    res = Excon.get "https://api.sphere.io/#{project_key}/orders/#{order_id}", :headers => head(access_token)
+    JSON.parse res.body
+  end
+
+  private
+
+  def self.head(t)
+    { 'Authorization' => "Bearer #{t}" }
+  end
+
 end
