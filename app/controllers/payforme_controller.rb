384ab9f3d2fca2bdfe3bef419ed3b3032adc1aa4
Hajo Eichler <hajo.eichler@commercetools.de>
2013-04-23 20:29:57 +0200

    First walk through from Pay4Me request to send mail.

--- app/controllers/payforme_controller.rb
+++ app/controllers/payforme_controller.rb
@@ -1,12 +1,18 @@
+require 'sphere'
+
 class PayformeController < ApplicationController
   respond_to :json
 
   def authorize
     e = params['email']
-    o = params['orderId']
+    o_id = params['orderId']
     p = params['projectKey']
     client_id = Settings.sphere.client_id
     client_secret = Settings.sphere.client_secret
+    t = Sphere.login client_id, client_secret, p
+    order = Sphere.get_order t, p, o_id
+    m = UserMailer.email_to_mom e, order
+    m.deliver
     respond_to do |format|
       format.json { render :json => 'OK' }
     end
--- spec/requests/payforme_spec.rb
+++ spec/requests/payforme_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 describe 'Payforme API' do
   it "get's the order from sphere" do
-    post '/payforme.json', :email => 'foo@example.com', :orderId => "123", :projectKey => 'p'
+    post '/payforme.json', :email => 'hajo@commercetools.de', :orderId => "c8f2424b-3731-4005-b1bc-a12bddf6a0f9", :projectKey => 'payforme'
     expect(response.status).to eq(200)
   end
 end
