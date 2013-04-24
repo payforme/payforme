50050576cb5e50f5d3b6367a06b7ccec13d8057f
Christian Zacharias <chz@pulni-work.local>
2013-04-24 15:07:10 +0200

    nicer price

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
     value = price.centAmount.to_f / 100
     sprintf("%5.2f #{price.currencyCode}", value)
   end
+
+  def nicer_price(price)
+    value = price.to_f / 100
+    sprintf("%5.2f", value)
+  end
 end
