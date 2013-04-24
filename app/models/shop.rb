aced5e8abc11c2eb542520f3fdcff7d0afc09247
Mathias Fiedler <mathias@musterdenker.de>
2013-04-24 13:49:52 +0200

    making the assignment of shop / payment possible

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,3 @@
 class Shop < ActiveRecord::Base
-  attr_accessible :name, :project_key, :sphere_token, :paymill_public_key, :paymill_private_key, :sphere_client_id, :sphere_client_secret
+  attr_accessible :name, :project_key, :sphere_token, :paymill_public_key, :paymill_private_key, :sphere_client_id, :sphere_client_secret, :payment_id
 end
